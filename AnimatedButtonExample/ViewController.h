b1eb04bd87e1d0e3a4e5dbe25057487f9a3370ad
Andrey Sapunov <asap@Andreys-Mac-mini.local>
2015-09-06 11:53:28 +0100

    Initial Commit

---
+++ AnimatedButtonExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatedButtonExample
+//
+//  Created by Andrey Sapunov on 06/09/15.
+//  Copyright (c) 2015 Andrey Sapunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
